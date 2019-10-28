ab4c879204b34314689de864e730093311962aeb
MaelleH <maelle.98@live.fr>
2019-10-28 15:51:26 +0100

    debut dao

---
+++ src/org/databaseManage/EmployeDAOImpl.java
@@ -0,0 +1,58 @@
+package org.databaseManage;
+import org.model.Employe;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeDAOImpl {
+	/**
+	 * common method used to query DB
+	 * 
+	 * @param query
+	 *            the SQL query to use
+	 * @return a list of Employes built from the SQL query
+	 */
+	private List<Employe> findBy(String query) {
+		Connection conn = null;
+		List<Employe> listEmployes = new ArrayList<Employe>();
+		Statement stat = null;
+		ResultSet rs = null;
+		try {
+			conn = DBManager.getInstance().getConnection();
+			if (conn != null) {
+				stat = conn.createStatement();
+				rs = stat.executeQuery(query);
+				while (rs.next()) {
+					listEmployes.add(new Employe(rs.getString("mail"),rs.getString("firstName"),rs.getString("surname"),rs.getString("address"),rs.getInt("nbDays")));
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception : main exception should thrown to servlet
+			// layer to display error message
+			e.printStackTrace();
+
+		} finally {
+			// always clean up all resources in finally block
+			if (conn != null) {
+				DBManager.getInstance().cleanup(conn, stat, rs);
+			}
+		}
+		return listEmployes;
+	}
+
+	public List<Employe> findByAll() {
+		// avoid select * queries because of performance issues,
+		// only query the columns you need
+		return findBy("select * from Employes");
+	}
+
+	public List<Employe> findByTitle(String searchText) {
+		// watch out : this query is case sensitive. use upper function on title
+		// and searchText to make it case insensitive
+		return findBy("select * from Employes where title like '%" + searchText + "%'");
+
+	}
+}
--- src/org/model/Employe.java
+++ src/org/model/Employe.java
@@ -7,4 +7,63 @@ public class Employe {
 	private String address;
 	private int nbDays;
 	
+	
+	public Employe(String mail, String firstName,String surname,String address, int nbDays) {
+		this.setMail(mail);
+		this.setFirstName(firstName);
+		this.setSurname(surname);
+		this.setAddress(address);
+		this.setNbDays(nbDays);
+	}
+
+
+	private String getAddress() {
+		return address;
+	}
+
+
+	private void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	private String getMail() {
+		return mail;
+	}
+
+
+	private void setMail(String mail) {
+		this.mail = mail;
+	}
+
+
+	private String getFirstName() {
+		return firstName;
+	}
+
+
+	private void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	private String getSurname() {
+		return surname;
+	}
+
+
+	private void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+
+	private int getNbDays() {
+		return nbDays;
+	}
+
+
+	private void setNbDays(int nbDays) {
+		this.nbDays = nbDays;
+	}
+	
 }
