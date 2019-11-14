ba870a5ce53c7b3c6fe784741791eb96c3ae2256
clevan <levan.clelia@live.com>
2019-11-14 15:27:57 +0100

    ajout date de naissance

--- BDD/Employe.sql
+++ BDD/Employe.sql
@@ -14,6 +14,7 @@ CREATE OR REPLACE TABLE employe (
   mail varchar(255) NOT NULL,
   firstName varchar(255) NOT NULL,
   surname varchar(255) NOT NULL,
+  birthDate varchar(255) NOT NULL,
   address varchar(255) NOT NULL,
   nbDays int(11) NOT NULL,
   fonction enum('Employe', 'EmployeRH', 'TeamLeader', 'RespoRH') NOT NULL,
@@ -28,10 +29,10 @@ CREATE OR REPLACE TABLE employe (
 -- Contenu de la table 'employe'
 --
 
-INSERT INTO employe (mail, firstName, surname, address, nbDays, fonction, pwd) VALUES ('mheyrend@enssat.fr', 'Maëlle', 'Heyrendt', '2 rue de Taiwan', 25, 'TeamLeader', 'mh');
-INSERT INTO employe (mail, firstName, surname, address, nbDays, fonction, pwd) VALUES ('clevan@enssat.fr', 'Clélia', 'Le van', '2 rue du Vietam', 25, 'RespoRH','cv');
-INSERT INTO employe (mail, firstName, surname, address, nbDays, fonction, pwd) VALUES ('a@a.a', 'a', 'a', 'a', 25, 'TeamLeader','a');
-INSERT INTO employe (mail, firstName, surname, address, nbDays, fonction, pwd) VALUES ('b@b.b', 'b', 'b', 'b', 45, 'RespoRH','b');
-INSERT INTO employe (mail, firstName, surname, address, nbDays, fonction, pwd) VALUES ('c@c.c', 'c', 'c', 'c', 45, 'EmployeRH','c');
-INSERT INTO employe (mail, firstName, surname, address, nbDays, fonction, pwd) VALUES ('d@d.d', 'd', 'd', 'd', 15, 'Employe','d');
-INSERT INTO employe (mail, firstName, surname, address, nbDays, fonction, pwd,team) VALUES ('e@e.e', 'e', 'e', 'e', 15, 'Employe','e',1);
+INSERT INTO employe (mail, firstName, surname, birthDate, address, nbDays, fonction, pwd) VALUES ('mheyrend@enssat.fr', 'Maëlle', 'Heyrendt','29-12-1998', '2 rue de Taiwan', 25, 'TeamLeader', 'mh');
+INSERT INTO employe (mail, firstName, surname, birthDate, address, nbDays, fonction, pwd) VALUES ('clevan@enssat.fr', 'Clélia', 'Le van','29-11-1996', '2 rue du Vietam', 25, 'RespoRH','cv');
+INSERT INTO employe (mail, firstName, surname, birthDate, address, nbDays, fonction, pwd) VALUES ('a@a.a', 'a', 'a','jj-mm-aaaa', 'a', 25, 'TeamLeader','a');
+INSERT INTO employe (mail, firstName, surname, birthDate, address, nbDays, fonction, pwd) VALUES ('b@b.b', 'b', 'b','jj-mm-aaaa', 'b', 45, 'RespoRH','b');
+INSERT INTO employe (mail, firstName, surname, birthDate, address, nbDays, fonction, pwd) VALUES ('c@c.c', 'c', 'c','jj-mm-aaaa', 'c', 45, 'EmployeRH','c');
+INSERT INTO employe (mail, firstName, surname, birthDate, address, nbDays, fonction, pwd) VALUES ('d@d.d', 'd', 'd','jj-mm-aaaa', 'd', 15, 'Employe','d');
+INSERT INTO employe (mail, firstName, surname, birthDate, address, nbDays, fonction, pwd,team) VALUES ('e@e.e', 'e', 'e','jj-mm-aaaa', 'e', 15, 'Employe','e',1);
