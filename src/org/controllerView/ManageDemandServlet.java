8c867a8c647fa047528c26133e223dd98252b2a2
Mickael Tisserand <mickaeltisserandimr@gmail.com>
2019-11-14 15:57:20 +0100

    Ajout employe avec mdp par defaut corrige V2

--- src/org/controllerView/ManageDemandServlet.java
+++ src/org/controllerView/ManageDemandServlet.java
@@ -32,6 +32,7 @@ public class ManageDemandServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		this.doProcess(req, resp);
+	
 	}
 	
 	@Override
