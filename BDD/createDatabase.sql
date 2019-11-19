7c9843da9f6bab3955e9ea0fca0362ffa4a49ca4
MaelleH <maelle.98@live.fr>
2019-11-19 18:10:02 +0100

    Script pour creer la db en 22

---
+++ BDD/createDatabase.sql
@@ -0,0 +1,8 @@
+DROP DATABASE DaysOffManager;
+CREATE DATABASE DaysOffManager;
+
+source ./BDD/demande.sql;
+source ./BDD/Employe.sql;
+source ./BDD/motif.sql;
+source ./BDD/Team.sql;
+source ./BDD/trigger.sql;
